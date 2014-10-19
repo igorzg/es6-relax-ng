491b7ba7faf4fc4d3d1351d58c112ecc2e5db674
igorzg <igor.zg1987@gmail.com>
2014-10-20 01:29:36 +0200

    Added demo, webserver config

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ es6-relax-ng  [![Build Status](https://travis-ci.org/igorzg/es6-relax-ng.svg?bra
 
 Relax ng simplifier + validation algorithm.
 This is dev version!
+
+##### Run:
+1. npm install
+2. gulp
+3. open http://localhost:8000/demo/index.html
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
 var gulp = require('gulp');
 var traceur = require('gulp-traceur');
-
+var webserver = require('gulp-webserver');
 /**
  * Default task
  */
-gulp.task('default', function () {
+gulp.task('es5', function () {
     return gulp.src('src/**/*.js')
         .pipe(traceur({
             modules: 'amd'
         }))
         .pipe(gulp.dest('dist'));
 });
+
+
+gulp.task('webserver', function() {
+    gulp.src('./')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            open: true
+        }));
+});
+
+
+gulp.task('default', ['es5', 'webserver']);
